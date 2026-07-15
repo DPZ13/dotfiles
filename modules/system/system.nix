fb5a059316b1efc779feca04c8b076ab5967a42b
DPZ13 <@POVEZLO.povezlo13@gmail.com>
2026-07-15 13:51:26 +0400

    minor fix

--- modules/system/system.nix
+++ modules/system/system.nix
@@ -3,6 +3,6 @@
 {
 	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 	time.timeZone = "Europe/Saratov";
-	i18n.defaultLocate = "ru_RU.UTF-8";
+	i18n.defaultLocale = "ru_RU.UTF-8";
 	system.stateVersion = "26.05"; # DO NOT TOUCH ! ! !
 }
