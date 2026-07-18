9ca37670c720d39049678be59da73929e04ee05b
DPZ13 <@POVEZLO.povezlo13@gmail.com>
2026-07-19 00:58:11 +0400

    Added home-manager Added home-manager in flake.nix, not be setup

--- flake.nix
+++ flake.nix
@@ -2,14 +2,26 @@
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		niripkg.url = "github:niri-wm/niri";
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
-	outputs = inputs@{ self, nixpkgs, niripkg, ... }: {
+	outputs = inputs@{ self, nixpkgs, niripkg, home-manager, ... }: {
 		nixosConfigurations = {
 			dpz = nixpkgs.lib.nixosSystem {
 				specialArgs = { inherit inputs; };
 				modules = [ 
 					./configuration.nix
 					./hosts/LOQ_15ARP9/hardware-configuration.nix
+					home-manager.nixosModules.default
+					{
+						home-manager = {
+							useGlobalPkgs = true;
+							useUserPackages = true;
+							users.dpz = ./home.nix;
+						}:
+					};
 				];
 			};
 		};
